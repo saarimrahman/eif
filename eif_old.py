43b300d56a077a7c64be11718e377af01b85ad99
Saarim Rahman <saarimrahman@berkeley.edu>
2021-12-07 19:00:45 -0800

    supervised isolation forest v0

--- eif_old.py
+++ eif_old.py
@@ -5,7 +5,7 @@ Our method allows for the slicing of the data to be done using hyperplanes with 
 
 """
 
-__author__ = 'Matias Carrasco Kind & Sahand Hariri'
+__author__ = "Matias Carrasco Kind & Sahand Hariri"
 import numpy as np
 import random as rn
 import os
@@ -13,10 +13,10 @@ import warnings
 from version import __version__
 
 
-def c_factor(n) :
+def c_factor(n):
     """
     Average path length of unsuccesful search in a binary search tree given n points
-    
+
     Parameters
     ----------
     n : int
@@ -26,9 +26,9 @@ def c_factor(n) :
     -------
     float
         Average path length of unsuccesful search in a BST
-        
+
     """
-    return 2.0*(np.log(n-1)+0.5772156649) - (2.0*(n-1.)/(n*1.0))
+    return 2.0 * (np.log(n - 1) + 0.5772156649) - (2.0 * (n - 1.0) / (n * 1.0))
 
 
 class iForest(object):
@@ -59,7 +59,8 @@ class iForest(object):
     compute_paths(X_in)
         Computes the anomaly score for data X_in
     """
-    def __init__(self, X, ntrees,  sample_size, limit=None, ExtensionLevel=0):
+
+    def __init__(self, X, ntrees, sample_size, limit=None, ExtensionLevel=0):
         """
         iForest(X, ntrees,  sample_size, limit=None, ExtensionLevel=0)
         Initialize a forest by passing in training data, number of trees to be used and the subsample size.
@@ -85,11 +86,13 @@ class iForest(object):
         self.Trees = []
         self.limit = limit
         self.exlevel = ExtensionLevel
-        self.CheckExtensionLevel()                                              # Extension Level check. See def for explanation.
+        self.CheckExtensionLevel()  # Extension Level check. See def for explanation.
         if limit is None:
-            self.limit = int(np.ceil(np.log2(self.sample)))                     # Set limit to the default as specified by the paper (average depth of unsuccesful search through a binary tree).
+            self.limit = int(
+                np.ceil(np.log2(self.sample))
+            )  # Set limit to the default as specified by the paper (average depth of unsuccesful search through a binary tree).
         self.c = c_factor(self.sample)
-        for i in range(self.ntrees):                                            # This loop builds an ensemble of iTrees (the forest).
+        for i in range(self.ntrees):  # This loop builds an ensemble of iTrees (the forest).
             ix = rn.sample(range(self.nobjs), self.sample)
             X_p = X[ix]
             self.Trees.append(iTree(X_p, 0, self.limit, exlevel=self.exlevel))
@@ -100,11 +103,13 @@ class iForest(object):
         """
         dim = self.X.shape[1]
         if self.exlevel < 0:
-            raise Exception("Extension level has to be an integer between 0 and "+ str(dim-1)+".")
-        if self.exlevel > dim-1:
-            raise Exception("Your data has "+ str(dim) + " dimensions. Extension level can't be higher than " + str(dim-1) + ".")
+            raise Exception("Extension level has to be an integer between 0 and " + str(dim - 1) + ".")
+        if self.exlevel > dim - 1:
+            raise Exception(
+                "Your data has " + str(dim) + " dimensions. Extension level can't be higher than " + str(dim - 1) + "."
+            )
 
-    def compute_paths(self, X_in = None):
+    def compute_paths(self, X_in=None):
         """
         compute_paths(X_in = None)
         Compute anomaly scores for all data points in a dataset X_in
@@ -122,13 +127,14 @@ class iForest(object):
         if X_in is None:
             X_in = self.X
         S = np.zeros(len(X_in))
-        for i in  range(len(X_in)):
+        for i in range(len(X_in)):
             h_temp = 0
             for j in range(self.ntrees):
-                h_temp += PathFactor(X_in[i],self.Trees[j]).path*1.0            # Compute path length for each point
-            Eh = h_temp/self.ntrees                                             # Average of path length travelled by the point in all trees.
-            S[i] = 2.0**(-Eh/self.c)                                            # Anomaly Score
+                h_temp += PathFactor(X_in[i], self.Trees[j]).path * 1.0  # Compute path length for each point
+            Eh = h_temp / self.ntrees  # Average of path length travelled by the point in all trees.
+            S[i] = 2.0 ** (-Eh / self.c)  # Anomaly Score
         return S
+
 
 class Node(object):
     """
@@ -153,7 +159,8 @@ class Node(object):
     ntype: str
         The type of the node: 'exNode', 'inNode'.
     """
-    def __init__(self, X, n, p, e, left, right, node_type = '' ):
+
+    def __init__(self, X, n, p, e, left, right, node_type=""):
         """
         Node(X, n, p, e, left, right, node_type = '' )
         Create a node in a given tree (iTree objectg)
@@ -175,12 +182,13 @@ class Node(object):
         """
         self.e = e
         self.size = len(X)
-        self.X = X # to be removed
+        self.X = X  # to be removed
         self.n = n
         self.p = p
         self.left = left
         self.right = right
         self.ntype = node_type
+
 
 class iTree(object):
 
@@ -218,7 +226,7 @@ class iTree(object):
         Builds the tree recursively from a given node. Returns a Node object.
     """
 
-    def __init__(self,X,e,l, exlevel=0):
+    def __init__(self, X, e, l, exlevel=0):
         """
         iTree(X, e, l, exlevel=0)
         Create a tree
@@ -236,17 +244,17 @@ class iTree(object):
         """
         self.exlevel = exlevel
         self.e = e
-        self.X = X                                                              #save data for now. Not really necessary.
+        self.X = X  # save data for now. Not really necessary.
         self.size = len(X)
         self.dim = self.X.shape[1]
-        self.Q = np.arange(np.shape(X)[1], dtype='int')                         # n dimensions
+        self.Q = np.arange(np.shape(X)[1], dtype="int")  # n dimensions
         self.l = l
-        self.p = None                                                           # Intercept for the hyperplane for splitting data at a given node.
-        self.n = None                                                           # Normal vector for the hyperplane for splitting data at a given node.
+        self.p = None  # Intercept for the hyperplane for splitting data at a given node.
+        self.n = None  # Normal vector for the hyperplane for splitting data at a given node.
         self.exnodes = 0
-        self.root = self.make_tree(X,e,l)                                       # At each node create a new tree, starting with root node.
+        self.root = self.make_tree(X, e, l)  # At each node create a new tree, starting with root node.
 
-    def make_tree(self,X,e,l):
+    def make_tree(self, X, e, l):
         """
         make_tree(X,e,l)
         Builds the tree recursively from a given node. Returns a Node object.
@@ -265,23 +273,37 @@ class iTree(object):
         Node object
         """
         self.e = e
-        if e >= l or len(X) <= 1:                                               # A point is isolated in traning data, or the depth limit has been reached.
+        if e >= l or len(X) <= 1:  # A point is isolated in traning data, or the depth limit has been reached.
             left = None
             right = None
             self.exnodes += 1
-            return Node(X, self.n, self.p, e, left, right, node_type = 'exNode')
-        else:                                                                   # Building the tree continues. All these nodes are internal.
+            return Node(X, self.n, self.p, e, left, right, node_type="exNode")
+        else:  # Building the tree continues. All these nodes are internal.
             mins = X.min(axis=0)
             maxs = X.max(axis=0)
-            idxs = np.random.choice(range(self.dim), self.dim-self.exlevel-1, replace=False)  # Pick the indices for which the normal vector elements should be set to zero acccording to the extension level.
-            self.n = np.random.normal(0,1,self.dim)                             # A random normal vector picked form a uniform n-sphere. Note that in order to pick uniformly from n-sphere, we need to pick a random normal for each component of this vector.
+            idxs = np.random.choice(
+                range(self.dim), self.dim - self.exlevel - 1, replace=False
+            )  # Pick the indices for which the normal vector elements should be set to zero acccording to the extension level.
+            self.n = np.random.normal(
+                0, 1, self.dim
+            )  # A random normal vector picked form a uniform n-sphere. Note that in order to pick uniformly from n-sphere, we need to pick a random normal for each component of this vector.
             self.n[idxs] = 0
-            self.p = np.random.uniform(mins,maxs)                               # Picking a random intercept point for the hyperplane splitting data.
-            w = (X-self.p).dot(self.n) < 0                                      # Criteria that determines if a data point should go to the left or right child node.
-            return Node(X, self.n, self.p, e,\
-            left=self.make_tree(X[w],e+1,l),\
-            right=self.make_tree(X[~w],e+1,l),\
-            node_type = 'inNode' )
+            self.p = np.random.uniform(
+                mins, maxs
+            )  # Picking a random intercept point for the hyperplane splitting data.
+            w = (X - self.p).dot(
+                self.n
+            ) < 0  # Criteria that determines if a data point should go to the left or right child node.
+            return Node(
+                X,
+                self.n,
+                self.p,
+                e,
+                left=self.make_tree(X[w], e + 1, l),
+                right=self.make_tree(X[~w], e + 1, l),
+                node_type="inNode",
+            )
+
 
 class PathFactor(object):
     """
@@ -302,7 +324,8 @@ class PathFactor(object):
         Given a tree, it finds the path a single data points takes.
 
     """
-    def __init__(self,x,itree):
+
+    def __init__(self, x, itree):
         """
         PathFactor(x, itree)
         Given a single tree (iTree objext) and a data point x = [x1,x2,...,xn], compute the legth of the path traversed by the point on the tree when it reaches an external node.
@@ -314,12 +337,12 @@ class PathFactor(object):
         itree : iTree object
             A single tree.
         """
-        self.path_list=[]
+        self.path_list = []
         self.x = x
         self.e = 0
-        self.path  = self.find_path(itree.root)
+        self.path = self.find_path(itree.root)
 
-    def find_path(self,T):
+    def find_path(self, T):
         """
         find_path(T)
         Given a tree, find the path for a single data point based on the splitting criteria stored at each node.
@@ -333,25 +356,27 @@ class PathFactor(object):
         int
             The depth reached by the data point.
         """
-        if T.ntype == 'exNode':
-            if T.size <= 1: return self.e
+        if T.ntype == "exNode":
+            if T.size <= 1:
+                return self.e
             else:
                 self.e = self.e + c_factor(T.size)
                 return self.e
         else:
-            p = T.p                                                             # Intercept for the hyperplane for splitting data at a given node.
-            n = T.n                                                             # Normal vector for the hyperplane for splitting data at a given node.
+            p = T.p  # Intercept for the hyperplane for splitting data at a given node.
+            n = T.n  # Normal vector for the hyperplane for splitting data at a given node.
 
             self.e += 1
 
-            if (self.x-p).dot(n) < 0:
-                self.path_list.append('L')
+            if (self.x - p).dot(n) < 0:
+                self.path_list.append("L")
                 return self.find_path(T.left)
             else:
-                self.path_list.append('R')
+                self.path_list.append("R")
                 return self.find_path(T.right)
 
-def all_branches(node, current=[], branches = None):
+
+def all_branches(node, current=[], branches=None):
     """
     Utility function used in generating a graph visualization. It returns all the branches of a given tree so they can be visualized.
 
@@ -364,13 +389,14 @@ def all_branches(node, current=[], branches = None):
     list
         list of branches that were reached.
     """
-    current = current[:node.e]
-    if branches is None: branches = []
-    if node.ntype == 'inNode':
-        current.append('L')
+    current = current[: node.e]
+    if branches is None:
+        branches = []
+    if node.ntype == "inNode":
+        current.append("L")
         all_branches(node.left, current=current, branches=branches)
         current = current[:-1]
-        current.append('R')
+        current.append("R")
         all_branches(node.right, current=current, branches=branches)
     else:
         branches.append(current)
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy
 from Cython.Distutils import build_ext
+
 try:
     from setuptools import setup, find_packages
     from setuptools.extension import Extension
@@ -19,26 +20,30 @@ extra_link_args = []
 libraries = []
 library_dirs = []
 include_dirs = []
-exec(open('version.py').read())
+exec(open("version.py").read())
 setup(
-    name='eif',
+    name="eif",
     version=__version__,
-    author='Matias Carrasco Kind , Sahand Hariri, Seng Keat Yeoh',
-    author_email='mcarras2@illinois.edu',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("eif",
-                 sources=["_eif.pyx", "eif.cxx"],
-                 include_dirs=[numpy.get_include()],
-                 extra_compile_args=['-std=c++11', '-Wcpp'],
-                 language="c++")],
+    author="Matias Carrasco Kind , Sahand Hariri, Seng Keat Yeoh",
+    author_email="mcarras2@illinois.edu",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        Extension(
+            "eif",
+            sources=["_eif.pyx", "eif.cxx"],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=["-std=c++11", "-Wcpp"],
+            language="c++",
+        )
+    ],
     scripts=[],
-    py_modules=['eif_old', 'version'],
+    py_modules=["eif_old", "version"],
     packages=[],
-    license='License.txt',
+    license="License.txt",
     include_package_data=True,
-    description='Extended Isolation Forest for anomaly detection',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    url='https://github.com/sahandha/eif',
+    description="Extended Isolation Forest for anomaly detection",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sahandha/eif",
     install_requires=["numpy", "cython"],
 )
